e39b5d9c215d00c6bf0631680a3eb60e68e26b37
Yakov Maman <yakovmaman3@gmail.com>
2025-04-24 00:38:09 +0300

    g

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -11,16 +11,15 @@ export default function ImageSlider({ images, interval = 3000 }) {
   }, [images.length, interval]);
 
   return (
-    <div className="relative w-full overflow-hidden rounded">
+    <div className="relative w-full overflow-hidden rounded h-[420px] flex items-center justify-center">
       {images.map((img, index) => (
         <img
           key={index}
           src={img}
           alt={`תמונה ${index + 1}`}
-          className={`transition-opacity duration-[1000ms] ease-[cubic-bezier(0.4,0,0.2,1)] w-full max-h-[500px] object-contain mx-auto ${
+          className={`transition-opacity duration-[1000ms] ease-[cubic-bezier(0.4,0,0.2,1)] w-auto max-h-full object-contain mx-auto ${
             index === current ? "opacity-100" : "opacity-0 absolute"
           }`}
-
         />
       ))}
     </div>
