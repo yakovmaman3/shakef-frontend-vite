f79d6b2b918fa1e0c348723afa97c0fefb539c08
Yakov Maman <yakovmaman3@gmail.com>
2025-04-23 21:22:55 +0300

    fix

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,6 +5,7 @@ import IntroSection from "./IntroSection";
 import HeavyWorkSection from "./HeavyWorkSection";
 import PremiumWorkSection from "./PremiumWorkSection";
 import VisionSection from "./VisionSection";
+import AboutSection from "./AboutSection";
 
 export default function HomePage({ activeSection, setActiveSection, setShowPhone }) {
   return (
@@ -20,26 +21,26 @@ export default function HomePage({ activeSection, setActiveSection, setShowPhone
         <HeroSection />
 
         <div className="backdrop-blur-md bg-black/30">
-          {/* כאן מועבר setShowPhone כמו שצריך */}
           <IntroSection setShowPhone={setShowPhone} />
           <HeavyWorkSection />
           <PremiumWorkSection />
           <VisionSection />
+          <AboutSection />
         </div>
 
         {/* Floating Buttons */}
         <div className="fixed bottom-4 right-4 z-50 flex flex-col gap-3">
           <a href="https://wa.me/972552270388" target="_blank" rel="noopener noreferrer">
             <div className="bg-green-500 rounded-full p-3 shadow-md hover:bg-green-600 transition">
-              <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M20.52 3.48a11.924 11.924 0 00-16.89 0 11.924 11.924 0 000 16.89l-1.36 4.03 4.03-1.36a11.924 11.924 0 0016.89-16.89zm-9.73 14.7c-2.4 0-4.63-.84-6.42-2.29-.1-.09-.13-.24-.07-.36l.91-2.09a.267.267 0 01.3-.15c.23.06.47.11.71.15.19.04.38.06.58.06.23 0 .44-.03.64-.08a.27.27 0 01.27.12c.48.7 1.18 1.24 1.95 1.55.15.07.33.01.42-.14.33-.54.72-1.03 1.16-1.46.22-.22.33-.51.33-.82 0-.28-.07-.56-.2-.8a.268.268 0 01.06-.3c.57-.59 1.23-1.14 1.97-1.63a.27.27 0 01.34.03 9.96 9.96 0 012.64 3.61.27.27 0 01-.06.31 10.025 10.025 0 01-7.38 3.2z"/>
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white" viewBox="0 0 448 512" fill="currentColor">
+                <path d="M380.9 97.1C339-2.6 221.4-28.8 133.7 33.3 68.4 81.6 43.7 169.5 72.7 244.6l-24.3 92.3a12 12 0 0 0 14.8 14.8l92.3-24.3c75.1 29 163 4.3 211.3-61 62.1-87.7 35.9-205.3-55.9-257.3z"/>
               </svg>
             </div>
           </a>
 
           <div onClick={() => setShowPhone(true)} className="bg-blue-600 rounded-full p-3 shadow-md hover:bg-blue-700 transition cursor-pointer">
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M3 5.882c0 7.623 6.495 13.118 14.118 13.118.842 0 1.651-.066 2.423-.189a1 1 0 00.772-1.498l-1.72-2.87a1 1 0 00-1.319-.382l-2.272 1.052a9.007 9.007 0 01-4.957-4.957l1.052-2.272a1 1 0 00-.382-1.319L6.687 3.687a1 1 0 00-1.498.772c-.123.772-.189 1.581-.189 2.423z" />
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white" viewBox="0 0 512 512" fill="currentColor">
+              <path d="M493.4 24.6l-104-24C370.4-.8 360 7.5 360 19v72.5C275.5 134.2 233.2 176.5 200.5 246.5c-16.1-6.7-31.5-15.3-46.2-25.7-14.7-10.4-27.9-21.9-39.5-34.3-10.5-11.3-25.2-18.5-40.7-19.5C38.5 165.1 0 203.6 0 251.5 0 309 39.1 352.4 89.6 371.5 104.1 377 118.3 381.5 132 385l0 71.1c0 11.4 10.4 19.8 20.9 15.5l104-24c6.7-1.6 11.1-7.8 10.2-14.6C255.4 370.7 398.5 227.6 507.9 35.4c3.9-6.4.7-14.7-6.2-17.2z"/>
             </svg>
           </div>
         </div>
