04986fb83dd7c5fdf6583544f5b6608766be6fcb
Yakov Maman <yakovmaman3@gmail.com>
2025-04-26 21:15:09 +0300

    h

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import Navbar from "./Navbar";
 import HeroSection from "./HeroSection";
@@ -14,6 +13,7 @@ export default function HomePage({ activeSection, setActiveSection, setShowPhone
         <Navbar setActiveSection={setActiveSection} setShowPhone={setShowPhone} />
       </div>
 
+      {/* Hero section with background image */}
       <div
         className="min-h-screen bg-fixed bg-center bg-cover pt-28 ios-fix"
         style={{
@@ -21,31 +21,30 @@ export default function HomePage({ activeSection, setActiveSection, setShowPhone
           backgroundSize: "cover",
           backgroundPosition: "center"
         }}
-
-
       >
         <HeroSection />
+      </div>
 
-        <div className="backdrop-blur-md bg-black/30">
-          <IntroSection setShowPhone={setShowPhone} />
-          <HeavyWorkSection />
-          <PremiumWorkSection />
-          <VisionSection />
-        </div>
+      {/* Content section with plain dark background */}
+      <div className="bg-[#4f4f4f]">
+        <IntroSection setShowPhone={setShowPhone} />
+        <HeavyWorkSection />
+        <PremiumWorkSection />
+        <VisionSection />
+      </div>
 
-        {/* Floating Buttons */}
-        <div className="fixed bottom-4 right-4 z-50 flex flex-col gap-3">
-          {/* וואטסאפ */}
-          <a href="https://wa.me/972552270388" target="_blank" rel="noopener noreferrer">
-            <div className="bg-green-500 rounded-full p-3 shadow-md hover:bg-green-600 transition">
-              <img src="/icons/whatsapp.svg" alt="וואטסאפ" className="w-6 h-6" />
-            </div>
-          </a>
-
-          {/* טלפון */}
-          <div onClick={() => setShowPhone(true)} className="bg-blue-600 rounded-full p-3 shadow-md hover:bg-blue-700 transition cursor-pointer">
-            <img src="/icons/phone.jpg" alt="טלפון" className="w-6 h-6 rounded-full" />
+      {/* Floating Buttons */}
+      <div className="fixed bottom-4 right-4 z-50 flex flex-col gap-3">
+        {/* וואטסאפ */}
+        <a href="https://wa.me/972552270388" target="_blank" rel="noopener noreferrer">
+          <div className="bg-green-500 rounded-full p-3 shadow-md hover:bg-green-600 transition">
+            <img src="/icons/whatsapp.svg" alt="וואטסאפ" className="w-6 h-6" />
           </div>
+        </a>
+
+        {/* טלפון */}
+        <div onClick={() => setShowPhone(true)} className="bg-blue-600 rounded-full p-3 shadow-md hover:bg-blue-700 transition cursor-pointer">
+          <img src="/icons/phone.jpg" alt="טלפון" className="w-6 h-6 rounded-full" />
         </div>
       </div>
     </div>
